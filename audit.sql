3442b5da6b6a3205c78615b5c306a53634ca2d08
NicolasFujisawa <nicolas.fujisawa@fatec.sp.gov.br>
2020-10-28 23:05:05 -0300

    new indent

--- audit.sql
+++ audit.sql
@@ -25,14 +25,15 @@ create table AUDIT_TIPO(
 	TIPO VARCHAR2(50)
 );
 
-insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(0, 'Inserindo');
-insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(1, 'Antes do update');
-insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(2, 'Depois do update');
-insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(3, 'Deletando');
 -- 0- insert
 -- 1- update before
 -- 2- update after
 -- 3- delete
+insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(0, 'Inserindo');
+insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(1, 'Antes do update');
+insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(2, 'Depois do update');
+insert into AUDIT_TIPO (AUDIT_TIPO, TIPO) values(3, 'Deletando');
+
 
 create table EMPLOYEE_AUD(
 	AUD  NUMBER NOT NULL,
@@ -53,59 +54,72 @@ create table EMPLOYEE_AUD(
 CREATE SEQUENCE aud_id_seq START WITH 0 INCREMENT BY 1;
 
 
-create or replace trigger AUDIT_EMPLOYEES
-after insert or update or delete on EMPLOYEES
-for each row
-declare
-	novo_id NUMBER;
-begin
-	novo_id := aud_id_seq.nextval;
-	insert into AUDIT_INFO values (novo_id, sysdate, user);
+CREATE OR REPLACE TRIGGER audit_employees AFTER
+    INSERT OR UPDATE OR DELETE ON employees
+    FOR EACH ROW
+
+DECLARE
+    novo_id NUMBER;
+
+BEGIN
+    novo_id := aud_id_seq.nextval;
+    INSERT INTO audit_info VALUES (
+        novo_id,
+        sysdate,
+        user
+    );
 
     /*Se está inserindo grava os nomes*/
-    if (INSERTING) then -- inserindo
+    IF ( inserting ) THEN -- inserindo
     	/* inserindo apenas os NOVOS valores na table, TIPO do ddl, AUD id do */
         insert into EMPLOYEE_AUD (AUD, AUDIT_TIPO, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID)
         	values (novo_id, 0, :new.EMPLOYEE_ID, :new.FIRST_NAME, 
         		:new.LAST_NAME, :new.EMAIL, :new.PHONE_NUMBER,
         		:new.HIRE_DATE, :new.JOB_ID, :new.SALARY, 
         		:new.MANAGER_ID , :new.DEPARTMENT_ID);  
-    end if;
+    END IF;
 
     /*atulizando registra os nomes olds e os novos*/
-    if (UPDATING) then -- atualizando
-        insert into EMPLOYEE_AUD (AUD, AUDIT_TIPO, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID)
+    IF ( updating ) THEN -- atualizando
+        INSERT INTO EMPLOYEE_AUD (AUD, AUDIT_TIPO, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID)
         	values (novo_id, 1, :old.EMPLOYEE_ID, :old.FIRST_NAME, 
         		:old.LAST_NAME, :old.EMAIL, :old.PHONE_NUMBER,
         		:old.HIRE_DATE, :old.JOB_ID, :old.SALARY,
         		:old.MANAGER_ID, :old.DEPARTMENT_ID);
 
-        insert into EMPLOYEE_AUD (AUD, AUDIT_TIPO, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID)
+        INSERT INTO EMPLOYEE_AUD (AUD, AUDIT_TIPO, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID)
         	values (novo_id, 2, :new.EMPLOYEE_ID, :new.FIRST_NAME, 
         		:new.LAST_NAME, :new.EMAIL, :new.PHONE_NUMBER,
         		:new.HIRE_DATE, :new.JOB_ID, :new.SALARY, 
         		:new.MANAGER_ID, :new.DEPARTMENT_ID);
-    end if;
+    END IF;
 
     /*e deletando apenas o ultimo registro da linha*/
-    if (DELETING) then -- deletando
-    	insert into EMPLOYEE_AUD (AUD, AUDIT_TIPO, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID)
+    IF ( deleting ) THEN -- deletando
+        INSERT INTO EMPLOYEE_AUD (AUD, AUDIT_TIPO, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID)
     		values (novo_id, 3, :old.EMPLOYEE_ID, :old.FIRST_NAME, 
         		:old.LAST_NAME, :old.EMAIL, :old.PHONE_NUMBER,
         		:old.HIRE_DATE, :old.JOB_ID, :old.SALARY, 
         		:old.MANAGER_ID, :old.DEPARTMENT_ID);
+    END IF;
 
-    end if;
+END;
 
-end;
 
-/*
-pontos chaves para explicar (se quiser) por slides:
- - trigger 1
- - aonde usar 1
- - aonde não usar 1
- - audit 1 ou 2
- - action 1
- - :novo & :old.. 1
- * código/pratica 5
- */
+CREATE TRIGGER enviar_boleto AFTER 
+	INSERT ON purchase_order
+	FOR EACH ROW
+BEGIN
+	utl_mail.send
+	(
+		sender =>
+			'company@email.com',
+		recipients =>
+			'someclient@email.com',
+		subject =>
+			'Novo pedido' || 
+			:new.po_number,
+			message => 
+				'pedido realizado, pague o boleto em anexo..'
+	);
+END;
